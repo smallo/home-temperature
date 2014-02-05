2ecf89c52c8252f793a27736862a7e6517bbcc3e
Sergio Mallo <sergiom@tid.es>
2014-02-05 01:25:57 +0100

    First working version in raspberry

--- temperature/readers.py
+++ temperature/readers.py
@@ -23,9 +23,6 @@ class TempReaderMock():
 
 class TempReaderDS18B20():
     def __init__(self):
-        os.system('modprobe w1-gpio')
-        os.system('modprobe w1-therm')
-        
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
         self.device_file = device_folder + '/w1_slave'
