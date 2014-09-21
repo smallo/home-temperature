96a4d1b71fad36c8a12d48991bfcae967bde8191
Marta Fuertes Deus <marta@MartaMac.local>
2014-09-21 18:34:53 +0200

    [FEATURE] Added temperature aggregates

--- temperature/models.py
+++ temperature/models.py
@@ -4,8 +4,27 @@ class Temperature(models.Model):
     timestamp = models.DateTimeField('Temperature sample time')
     value = models.FloatField()
     
+class TemperatureHourly(models.Model):
+    hour = models.CharField(max_length=10)
+    n_samples = models.IntegerField()
+    sum_value = models.FloatField()
+    min_value = models.FloatField()
+    max_value = models.FloatField()
+    
+class TemperatureDaily(models.Model):
+    day = models.CharField(max_length=8)
+    n_samples = models.IntegerField()
+    sum_value = models.FloatField()
+    min_value = models.FloatField()
+    max_value = models.FloatField()
+    
+class TemperatureMonthly(models.Model):
+    day = models.CharField(max_length=6)
+    n_samples = models.IntegerField()
+    sum_value = models.FloatField()
+    min_value = models.FloatField()
+    max_value = models.FloatField()
 
 class Configuration(models.Model):
     key = models.CharField(max_length=25)
     value = models.CharField(max_length=200)
-
--- temperature/readers.py
+++ temperature/readers.py
@@ -7,7 +7,7 @@ Created on 18/01/2014
 import random
 import os, glob, time
 
-from models import Temperature
+from models import Temperature, TemperatureHourly, TemperatureDaily, TemperatureMonthly
 from datetime import datetime
 
 
@@ -27,17 +27,17 @@ class TempReaderDS18B20():
         device_folder = glob.glob(base_dir + '28*')[0]
         self.device_file = device_folder + '/w1_slave'
         
-    def read_temp_raw(self):
+    def _read_temp_raw(self):
         f = open(self.device_file, 'r')
         lines = f.readlines()
         f.close()
         return lines
      
     def get_temp(self):
-        lines = self.read_temp_raw()
+        lines = self._read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = self.read_temp_raw()
+            lines = self._read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos+2:]
@@ -48,6 +48,62 @@ class TempReaderDS18B20():
 
 def take_sample():
     reader = TempReaderDS18B20()
-    temp = reader.get_temp()
+    temperature = reader.get_temp()
     time = datetime.now()
-    Temperature.objects.create(timestamp=time, value=temp)
+    Temperature.objects.create(timestamp=time, value=temperature)
+
+    # Update hourly aggregate
+    hour = time.strftime('%Y%m%d%H')
+    hour_obj = TemperatureHourly.objects.filter(hour=hour)
+    if len(hour_obj == 0):
+        # This is the first measure we have for this hour
+        TemperatureHourly.objects.create(hour=hour, n_samples=1,
+                average_value=temperature, min_value=temperature, max_value=temperature)
+    elif:
+        # Let's update the value
+        hour_obj = hour_obj[0]
+        hour_obj.n_samples += 1
+        hour_obj.sum_value += temperature
+        if temperature < hour_obj.min_value:
+            hour_obj.min_value = temperature
+        if temperature > hour_obj.max_value:
+            hour_obj.max_value = temperature
+        hour_obj.save()
+
+    # Update daily aggregate
+    # TODO: this is duplicated code
+    day = time.strftime('%Y%m%d')
+    day_obj = TemperatureDaily.objects.filter(day=day)
+    if len(day_obj == 0):
+        # This is the first measure we have for this day
+        TemperatureDaily.objects.create(day=day, n_samples=1,
+                average_value=temperature, min_value=temperature, max_value=temperature)
+    elif:
+        # Let's update the value
+        day_obj = day_obj[0]
+        day_obj.n_samples += 1
+        day_obj.sum_value += temperature
+        if temperature < day_obj.min_value:
+            day_obj.min_value = temperature
+        if temperature > day_obj.max_value:
+            day_obj.max_value = temperature
+        day_obj.save()
+
+    # Update month aggregate
+    # TODO: this is duplicated code
+    month = time.strftime('%Y%m%d')
+    month_obj = TemperatureMonthly.objects.filter(month=month)
+    if len(month_obj == 0):
+        # This is the first measure we have for this month
+        TemperatureMonthly.objects.create(month=month, n_samples=1,
+                average_value=temperature, min_value=temperature, max_value=temperature)
+    elif:
+        # Let's update the value
+        month_obj = month_obj[0]
+        month_obj.n_samples += 1
+        month_obj.sum_value += temperature
+        if temperature < month_obj.min_value:
+            month_obj.min_value = temperature
+        if temperature > month_obj.max_value:
+            month_obj.max_value = temperature
+        month_obj.save()
